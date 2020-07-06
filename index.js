9218bff13d63c116564254b2eec0bb7f5b6883fb
Vindymuasa <s21810184@student.unklab.ac.id>
2020-07-06 15:19:23 +0800

    edit cv

---
+++ index.js
@@ -0,0 +1,17 @@
+let ayamImg = document.getElementsById("ayam-img");
+
+console.log(ayamImg.src);
+console.log(ayam.alt);
+console.log(ayam.width);
+console.log(ayam.height);
+
+
+let demo = document.getElementsById("demo");
+demo.color="black";
+console.log(demo.color);
+
+let coaladanpanda = document.getElementsByid("coaladanpanda");
+coaladanpanda.color ="black";
+coaladanpanda.fontSize ="10";
+console.log(coaladanpanda.color);
+console.log(coaladanpanda.fontSize);
